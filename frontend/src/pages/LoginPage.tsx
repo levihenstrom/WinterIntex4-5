81ea656f3473032111fc26ea0e39e006ad5a5318
Levihenny <levi.henstrom@gmail.com>
2026-04-01 21:30:47 -0600

    Commit made from VS Code Azure Static Web Apps

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import type { FormEvent } from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import {
